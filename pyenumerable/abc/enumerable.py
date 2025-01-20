3cc66e684162dfad69d5cfe70ee51c8f6fc2be84
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-01-20 21:07:32 +0330

    add abstract methods for all, any, add, average, cast, chunk, concat, contains, count & distinct operations

--- pyenumerable/abc/enumerable.py
+++ pyenumerable/abc/enumerable.py
@@ -1,33 +1,66 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
-from collections.abc import Callable, Collection, Mapping, Set
+from collections.abc import (
+    Callable,
+    Collection,
+    Hashable,
+    Mapping,
+    Sequence,
+    Set,
+)
 
 
-class Enumerable[T](ABC, Collection[T]):
+class Enumerable[T: Hashable](ABC, Collection[T]):
+
+    "corresponding to .NET 8"
 
     @abstractmethod
-    def to_mapping[TK](
-        self,
-        key_selector: Callable[[T], TK],
-    ) -> Mapping[TK, T]: ...
+    def to_mapping[TK](self, func: Callable[[T], TK]) -> Mapping[TK, T]: ...
 
     @abstractmethod
     def to_set(self) -> Set[T]: ...
 
     @abstractmethod
-    def aggregate[TA](self, accumulator: Callable[[T, T], TA]) -> TA: ...
-
-    @abstractmethod
-    def aggregate_with_seed[TA](
+    def aggregate[TA](
         self,
-        seed: TA,
-        accumulator: Callable[[TA, T], TA],
+        func: Callable[[T, T], TA] | Callable[[TA, T], TA],
+        seed: TA | None = None,
     ) -> TA: ...
 
     @abstractmethod
-    def aggregate_with_seed_and_selector[TA, TS](
-        self,
-        seed: TA,
-        accumulator: Callable[[TA, T], TA],
-        selector: Callable[[TA], TS],
-    ) -> TS: ...
+    def all(self, func: Callable[[T], bool]) -> bool: ...
 
+    @abstractmethod
+    def any(self, func: Callable[[T], bool] | None = None) -> bool: ...
+
+    @abstractmethod
+    def add(self, item: T) -> Enumerable[T]: ...
+
+    @abstractmethod
+    def average[TA: (int, float)](self, func: Callable[[T], TA]) -> TA: ...
+
+    @abstractmethod
+    def cast[TO](self, type_: type[TO]) -> Enumerable[TO]: ...
+
+    @abstractmethod
+    def chunk(self, size: int) -> Sequence[Enumerable[T]]: ...
+
+    @abstractmethod
+    def concat(self, other: Enumerable[T]) -> Enumerable[T]: ...
+
+    @abstractmethod
+    def contains(
+        self,
+        item: T,
+        comparer: Callable[[T, T], bool] | None = None,
+    ) -> bool: ...
+
+    @abstractmethod
+    def count(self, func: Callable[[T], bool] | None = None) -> int: ...
+
+    @abstractmethod
+    def distinct(
+        self,
+        comparer: Callable[[T, T], bool] | None = None,
+    ) -> Enumerable[T]: ...
