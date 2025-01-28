1d2b829d776a0a1cc55d9db1411e76063295afd7
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-01-29 00:02:05 +0330

    add typing utilites for comparer functions and comparable types

---
+++ pyenumerable/typing_utility.py
@@ -0,0 +1,16 @@
+from collections.abc import Callable
+from typing import Protocol
+
+type Comparer[T] = Callable[[T,T], bool]
+
+
+class Comparable(Protocol):
+    def __eq__(self, other: object, /) -> bool: ...
+
+    def __lt__(self, other: object, /) -> bool: ...
+
+    def __gt__(self, other: object, /) -> bool: ...
+
+    def __le__(self, other: object, /) -> bool: ...
+
+    def __ge__(self, other: object, /) -> bool: ...
