8e73a9c9ee15b522aaf1fa043f910f7c65d63d1f
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-02-14 17:03:41 +0330

    add select many method to supports select protocol

--- pyenumerable/protocol/_supports_select.py
+++ pyenumerable/protocol/_supports_select.py
@@ -1,12 +1,17 @@
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from typing import TYPE_CHECKING, Protocol
 
 if TYPE_CHECKING:
-    from _queryable import Queryable
+    from ._queryable import Queryable
 
 
 class SupportsSelect[TSource](Protocol):
     def select[TResult](
         self,
         selector: Callable[[int, TSource], TResult],
+    ) -> "Queryable[TResult]": ...
+
+    def select_many[TResult](
+        self,
+        selector: Callable[[int, TSource], Iterable[TResult]],
     ) -> "Queryable[TResult]": ...
