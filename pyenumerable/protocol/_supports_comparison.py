d4fab9f547aab4b284b9e986294a75444ab828f6
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-02-14 18:06:19 +0330

    add supports comparison protocol

---
+++ pyenumerable/protocol/_supports_comparison.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING, Protocol, overload
+
+from pyenumerable.typing_utility import Comparer
+
+if TYPE_CHECKING:
+    from ._queryable import Queryable
+
+
+class SupportsComparison[TSource](Protocol):
+    @overload
+    def sequence_equal(self, other: "Queryable[TSource]", /) -> bool: ...
+
+    @overload
+    def sequence_equal(
+        self,
+        other: "Queryable[TSource]",
+        /,
+        *,
+        comparer: Comparer[TSource],
+    ) -> bool: ...
