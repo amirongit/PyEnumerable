20e46866f76d5cb3183caf471c0ca465190f3b3c
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-02-14 09:50:50 +0330

    add protocol for min operations

---
+++ pyenumerable/protocol/_supports_minimum.py
@@ -0,0 +1,44 @@
+from collections.abc import Callable
+from typing import Protocol, overload
+
+from pyenumerable.typing_utility import Comparable, Comparer
+
+
+class SupportsMinimum[TSource](Protocol):
+    @overload
+    def min_(self, /) -> TSource: ...
+
+    @overload
+    def min_(self, /, *, comparer: Comparer[TSource]) -> TSource: ...
+
+    @overload
+    def min_[TResult](
+        self,
+        selector: Callable[[TSource], TResult],
+        /,
+    ) -> TResult: ...
+
+    @overload
+    def min_[TResult](
+        self,
+        selector: Callable[[TSource], TResult],
+        /,
+        *,
+        comparer: Comparer[TResult],
+    ) -> TResult: ...
+
+    @overload
+    def min_by[TKey: Comparable](
+        self,
+        key_selector: Callable[[TSource], TKey],
+        /,
+    ) -> TSource: ...
+
+    @overload
+    def min_by[TKey](
+        self,
+        key_selector: Callable[[TSource], TKey],
+        /,
+        *,
+        comparer: Comparer[TKey],
+    ) -> TSource: ...
