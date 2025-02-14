9e02c9fdb5d4aca9669ace5509447e05f0776130
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-02-14 17:31:26 +0330

    add more select many operation overloads

--- pyenumerable/protocol/_supports_select.py
+++ pyenumerable/protocol/_supports_select.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable, Iterable
-from typing import TYPE_CHECKING, Protocol
+from typing import TYPE_CHECKING, Protocol, overload
 
 if TYPE_CHECKING:
     from ._queryable import Queryable
@@ -9,9 +9,23 @@ class SupportsSelect[TSource](Protocol):
     def select[TResult](
         self,
         selector: Callable[[int, TSource], TResult],
+        /,
     ) -> "Queryable[TResult]": ...
 
+    @overload
     def select_many[TResult](
         self,
         selector: Callable[[int, TSource], Iterable[TResult]],
+        /,
+    ) -> "Queryable[TResult]": ...
+
+    @overload
+    def select_many[TResult, TCollectionItem](
+        self,
+        iterable_selector: Callable[
+            [int, TSource],
+            Iterable[TCollectionItem],
+        ],
+        result_selector: Callable[[TSource, TCollectionItem], TResult],
+        /,
     ) -> "Queryable[TResult]": ...
