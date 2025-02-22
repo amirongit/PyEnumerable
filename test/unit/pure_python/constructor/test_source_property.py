14511c345b2fece8839578a69f2069129c006372
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-02-22 15:50:30 +0330

    make tests use warlus operator

--- test/unit/pure_python/constructor/test_source_property.py
+++ test/unit/pure_python/constructor/test_source_property.py
@@ -1,26 +1,54 @@
 from random import choice
 
-from pyenumerable.implementations.pure_python import Enumerable
+from pyenumerable.implementations.pure_python import PurePythonEnumerable
 from test.unit.pure_python.test_utility import generate_random_args
 
 
 class TestEnumerableConstructor:
     def test_initialization_with_raw_items(self) -> None:
-        args = generate_random_args(choice(range(100)), range(-25, 25))
-        obj = Enumerable(*args)
+        obj = PurePythonEnumerable(
+            *(
+                items := generate_random_args(
+                    choice(range(100)),
+                    range(-25, 25),
+                )
+            ),
+        )
 
-        assert args == obj.source
+        assert items == obj.source
 
     def test_initialization_with_packed_items(self) -> None:
-        first_pack = generate_random_args(choice(range(10)), range(-25, 25))
-        second_pack = generate_random_args(choice(range(10)), range(-25, 25))
-        obj = Enumerable(from_iterables=((first_pack, second_pack)))
+        obj = PurePythonEnumerable(
+            from_iterables=(
+                (
+                    first := generate_random_args(
+                        choice(range(10)),
+                        range(-25, 25),
+                    ),
+                    second := generate_random_args(
+                        choice(range(10)),
+                        range(-25, 25),
+                    ),
+                )
+            ),
+        )
 
-        assert first_pack + second_pack == obj.source
+        assert first + second == obj.source
 
     def test_initialization_with_raw_and_packed_items(self) -> None:
-        raw_items = generate_random_args(choice(range(10)), range(-25, 25))
-        second_pack = generate_random_args(choice(range(10)), range(-25, 25))
-        obj = Enumerable(*raw_items, from_iterables=(second_pack,))
+        obj = PurePythonEnumerable(
+            *(
+                raw := generate_random_args(
+                    choice(range(10)),
+                    range(-25, 25),
+                )
+            ),
+            from_iterables=(
+                packed := generate_random_args(
+                    choice(range(10)),
+                    range(-25, 25),
+                ),
+            ),
+        )
 
-        assert raw_items + second_pack == obj.source
+        assert raw + packed == obj.source
--- test/unit/pure_python/select/test_select_method.py
+++ test/unit/pure_python/select/test_select_method.py
@@ -1,39 +1,28 @@
 from collections.abc import Callable
 from random import choice
 
-from pyenumerable.implementations.pure_python import Enumerable
+from pyenumerable.implementations.pure_python import PurePythonEnumerable
 from test.unit.pure_python.test_utility import generate_random_args
 
 
 class TestSelectMethod:
-    def test_with_index(self) -> None:
-        obj = Enumerable(
-            *generate_random_args(choice(range(100)), range(-25, 25)),
+    def test_functionality(self) -> None:
+        obj = PurePythonEnumerable(
+            *(
+                items := generate_random_args(
+                    choice(range(100)),
+                    range(-25, 25),
+                )
+            ),
         )
 
         res = obj.select(TestSelectMethod.combine_index_and_value)
 
         assert res.source == tuple(
             TestSelectMethod.combine_index_and_value(i, v) for i,
-            v in enumerate(obj.source)
-        )
-
-    def test_without_index(self) -> None:
-        obj = Enumerable(
-            *generate_random_args(choice(range(100)), range(-25, 25)),
-        )
-
-        res = obj.select(TestSelectMethod.raise_to_the_power_of_two)
-
-        assert res.source == tuple(
-            TestSelectMethod.raise_to_the_power_of_two(i, v) for i,
-            v in enumerate(obj.source)
+            v in enumerate(items)
         )
 
     @staticmethod
     def combine_index_and_value(index: int, value: int) -> str:
         return f"{index}: {value}"
-
-    @staticmethod
-    def raise_to_the_power_of_two(_: int, value: int) -> int:
-        return value ** 2
