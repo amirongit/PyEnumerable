4eec2432724294b4a78cee9f7042bab3e42fc5e9
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-02-22 15:50:41 +0330

    implement select many method

--- pyenumerable/implementations/pure_python/_enumerable.py
+++ pyenumerable/implementations/pure_python/_enumerable.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Iterable
+from itertools import chain
+from typing import Protocol, cast
 
 
-class Enumerable[TSource]:
+class PurePythonEnumerable[TSource]:
+
+    # TODO
+    # change return types to pyenumerable.protocol.Enumerable
+
     def __init__(
         self,
         *items: TSource,
@@ -12,8 +18,7 @@ class Enumerable[TSource]:
         self._source: tuple[TSource, ...] = items
 
         if from_iterables is not None:
-            for iterable in from_iterables:
-                self._source += tuple(iterable)
+            self._source += tuple(chain(*from_iterables))
 
     @property
     def source(self) -> tuple[TSource, ...]:
@@ -23,7 +28,16 @@ class Enumerable[TSource]:
         self,
         selector: Callable[[int, TSource], TResult],
         /,
-    ) -> Enumerable[TResult]:
-        return Enumerable(
+    ) -> PurePythonEnumerable[TResult]:
+        return PurePythonEnumerable(
             *tuple(selector(i, v) for i, v in enumerate(self.source)),
+       )
+
+    def select_many[TResult](
+        self,
+        selector: Callable[[int, TSource], Iterable[TResult]],
+        /,
+    ) -> PurePythonEnumerable[TResult]:
+        return PurePythonEnumerable(
+            from_iterables=[selector(i, v) for i, v in enumerate(self.source)],
         )
