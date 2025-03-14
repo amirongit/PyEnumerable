8b5af90dcc3afd098302f2caddf8a5d87ab008bb
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-03-14 11:56:04 +0330

    modif supports all protocol to have a predicate less signature

--- test/unit/pure_python/all/test_all_method.py
+++ test/unit/pure_python/all/test_all_method.py
@@ -3,23 +3,42 @@ from test.unit.pure_python.test_utility import Person
 
 
 class TestAllMethod:
-    def test_when_empty(self) -> None:
+    def test_without_predicate_when_empty(self) -> None:
+        obj: PurePythonEnumerable[int] = PurePythonEnumerable()
+
+        res = obj.all()
+
+        assert res is True
+
+    def test_pass_without_predicate(self) -> None:
+        obj = PurePythonEnumerable(True, True, True)  # noqa: FBT003
+
+        res = obj.all()
+
+        assert res is True
+
+    def test_fail_without_predicate(self) -> None:
+        obj = PurePythonEnumerable(True, None, False)  # noqa: FBT003
+
+        res = obj.all()
+
+        assert res is False
+
+    def test_with_predicate_when_empty(self) -> None:
         obj: PurePythonEnumerable[int] = PurePythonEnumerable()
 
         res = obj.all(lambda _: False)
 
         assert res is True
 
-    def test_pass(self) -> None:
-        obj: PurePythonEnumerable[int] = PurePythonEnumerable(
-            *range(threshold := 7),
-        )
+    def test_pass_with_predicate(self) -> None:
+        obj = PurePythonEnumerable(*range(threshold := 7))
 
         res = obj.all(lambda x: x <= threshold)
 
         assert res is True
 
-    def test_fail(self) -> None:
+    def test_fail_with_predicate(self) -> None:
         obj = PurePythonEnumerable(
             Person("john", 14, Person("marry", 36)),
             Person("junior", 14, Person("james", 36)),
