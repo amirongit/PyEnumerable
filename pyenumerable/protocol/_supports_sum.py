84aef1761fd51874c893ef96779d36edb04786b0
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-03-14 12:59:54 +0330

    remove sum override with selector because python does not have conditional generics

--- pyenumerable/protocol/_supports_sum.py
+++ pyenumerable/protocol/_supports_sum.py
@@ -1,16 +1,5 @@
-from collections.abc import Callable
-from typing import Protocol, overload
-
-from pyenumerable.typing_utility import Addable
+from typing import Protocol
 
 
 class SupportsSum[TSource](Protocol):
-    @overload
     def sum(self, /) -> TSource: ...
-
-    @overload
-    def sum[TItem: Addable](
-        self,
-        selector: Callable[[TSource], TItem],
-        /,
-    ) -> TItem: ...
