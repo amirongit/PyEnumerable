08cbfab4276da3b0eba907f48d7838d90a60182b
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-03-15 12:58:17 +0330

    add unit tests for supports distinct protocol methods

---
+++ test/unit/pure_python/distinct/test_distinct_by_method.py
@@ -0,0 +1,52 @@
+import pytest
+
+from pyenumerable.implementations.pure_python import PurePythonEnumerable
+from test.unit.pure_python.test_utility import Person, Point
+
+
+class TestDistinctByMethod:
+    def test_when_empty(self) -> None:
+        obj: PurePythonEnumerable[int] = PurePythonEnumerable()
+
+        res = obj.distinct_by(lambda x: x ** 2)
+
+        assert res.source == ()
+
+    def test_exc_raise_when_unhashable(self) -> None:
+        obj = PurePythonEnumerable(
+            Person("john doe", 12),
+            Person("jane doe", 12, Person("marry doe", 36)),
+            Person("junior doe", 12, Person("larry doe", 28)),
+        )
+
+        with pytest.raises(TypeError):
+            obj.distinct_by(lambda person: person.parent)
+
+    def test_without_comparer(self) -> None:
+        obj = PurePythonEnumerable(
+            from_iterable=(items := tuple(range(7)), [-i for i in items]),
+        )
+
+        res = obj.distinct_by(lambda i: i ** 2)
+
+        assert res.source == items
+
+    def test_with_comparer(self) -> None:
+        obj = PurePythonEnumerable(
+            first := Point(3, 4),
+            Point(1, -4),
+            second := Point(3, 5),
+            Point(4, -5),
+            third := Point(3, 6),
+            Point(0, -6),
+            fourth := Point(3, 7),
+            Point(10, -7),
+        )
+
+        res = obj.distinct_by(
+            lambda point: point.y,
+            comparer=lambda first_y, second_y:
+            first_y ** 2 == second_y ** 2
+        )
+
+        assert res.source == (first, second, third, fourth)
---
+++ test/unit/pure_python/distinct/test_distinct_method.py
@@ -0,0 +1,50 @@
+import pytest
+
+from pyenumerable.implementations.pure_python import PurePythonEnumerable
+from test.unit.pure_python.test_utility import Person, Point
+
+
+class TestDistinctMethod:
+    def test_when_empty(self) -> None:
+        obj: PurePythonEnumerable[int] = PurePythonEnumerable()
+
+        res = obj.distinct()
+
+        assert res.source == ()
+
+    def test_exc_raise_when_unhashable(self) -> None:
+        obj = PurePythonEnumerable(
+            Person("john doe", 12),
+            Person("jane doe", 12),
+        )
+
+        with pytest.raises(TypeError):
+            obj.distinct()
+
+    def test_without_comparer(self) -> None:
+        obj = PurePythonEnumerable(
+            from_iterable=(items := tuple(range(7)), items),
+        )
+
+        res = obj.distinct()
+
+        assert res.source == items
+
+    def test_with_comparer(self) -> None:
+        obj = PurePythonEnumerable(
+            first := Point(3, 4),
+            Point(1, 4),
+            second := Point(3, 5),
+            Point(4, 5),
+            third := Point(3, 6),
+            Point(0, 6),
+            fourth := Point(3, 7),
+            Point(10, 7),
+        )
+
+        res = obj.distinct(
+            comparer=lambda first_point, second_point:
+            first_point.y == second_point.y,
+        )
+
+        assert res.source == (first, second, third, fourth)
