feee915261995babdddb7c0d0a3c3cf618908304
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-03-18 23:07:51 +0330

    ruff format

--- test/unit/pure_python/zip/test_zip_method.py
+++ test/unit/pure_python/zip/test_zip_method.py
@@ -4,7 +4,9 @@ from pyenumerable.implementations.pure_python import PurePythonEnumerable
 class TestZipMethod:
     def test_zip(self) -> None:
         first_object = PurePythonEnumerable(*(first_items := tuple(range(7))))
-        second_object = PurePythonEnumerable(*(second_items := tuple(range(7, 16))))
+        second_object = PurePythonEnumerable(
+            *(second_items := tuple(range(7, 16)))
+        )
 
         res = first_object.zip(second_object)
 
