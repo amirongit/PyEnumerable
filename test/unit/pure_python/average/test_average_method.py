f6b7d0eec55242fdd01861c22df24d8e39d13ac9
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-03-19 22:50:32 +0330

    add unit tests for average method

---
+++ test/unit/pure_python/average/test_average_method.py
@@ -0,0 +1,17 @@
+import pytest
+from pyenumerable.implementations.pure_python import PurePythonEnumerable
+
+
+class TestAverageMethod:
+    def test_average(self) -> None:
+        obj = PurePythonEnumerable(*(items := tuple(range(7))))
+
+        res = obj.average()
+
+        assert res == sum(items) / len(items)
+
+    def test_exc_raise_when_unexecutable(self) -> None:
+        obj = PurePythonEnumerable("should", "not", "work")
+
+        with pytest.raises(TypeError):
+            obj.average()
