0efc083d185f4966c3351b4be56973721c97548c
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-03-19 22:30:28 +0330

    add unit tests for supports except protocol methods

---
+++ test/unit/pure_python/except_/test_except_by_method.py
@@ -0,0 +1,45 @@
+from pyenumerable.implementations.pure_python import PurePythonEnumerable
+from test.unit.pure_python.test_utility import Point
+
+
+class TestExceptByMethod:
+    def test_with_comparer(self) -> None:
+        first_object = PurePythonEnumerable(
+            Point(0, 1),
+            first := Point(3, 2),
+            Point(4, 5),
+            second := Point(7, 6)
+        )
+        second_object = PurePythonEnumerable(
+            Point(3, 5),
+            Point(8, 9),
+            Point(-1, 1),
+            Point(4, 7)
+        )
+
+        res = first_object.except_by(second_object, lambda point: point.y)
+
+        assert res.source == (first, second)
+
+    def test_without_comparer(self) -> None:
+        first_object = PurePythonEnumerable(
+            first := Point(0, 2),
+            Point(9, 7),
+            second := Point(3, 1),
+            Point(4, -4)
+        )
+        second_object = PurePythonEnumerable(
+            Point(5, 4),
+            Point(8, 9),
+            Point(-1, 3),
+            Point(4, -7)
+        )
+
+        res = first_object.except_by(
+            second_object,
+            lambda point: point.y,
+            comparer=lambda first_y,
+            second_y: abs(first_y) == abs(second_y)
+        )
+
+        assert res.source == (first, second)
