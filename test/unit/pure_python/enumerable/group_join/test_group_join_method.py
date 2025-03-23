c26ac27943d3f7b572c5c6deba39fc71717b0e7c
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-03-23 19:47:18 +0330

    add unit tests for group join method

---
+++ test/unit/pure_python/enumerable/group_join/test_group_join_method.py
@@ -0,0 +1,52 @@
+from pyenumerable.implementations.pure_python import PurePythonEnumerable
+from test.unit.pure_python.test_utility import Person
+
+
+class TestGroupJoinMethod:
+    def test_group_join(self) -> None:
+        first_object = PurePythonEnumerable(
+            first_parent := Person("john doe", 32),
+            second_parent := Person("jane doe", 28),
+        )
+        second_object = PurePythonEnumerable(
+            first_child := Person("larry doe", 12, first_parent),
+            second_child := Person("jerry doe", 13, first_parent),
+            third_child := Person("marry doe", 14, second_parent),
+            fourth_child := Person("james doe", 15, second_parent),
+            fifth_child := Person("james doe", 16, second_parent),
+        )
+
+        res = first_object.group_join(
+            second_object,
+            lambda parent: parent.age,
+            lambda child: child.parent.age
+            if child.parent is not None
+            else None,
+            lambda parent, children: (parent.age, children.source),
+        )
+
+        assert res.source == (
+            (first_parent.age, (first_child, second_child)),
+            (second_parent.age, (third_child, fourth_child, fifth_child)),
+        )
+
+    def test_overlap_remove(self) -> None:
+        first_object = PurePythonEnumerable(
+            first_parent := Person("john doe", 32),
+            Person("jane doe", 32),
+        )
+        second_object = PurePythonEnumerable(
+            Person("larry doe", 12, first_parent),
+            Person("jerry doe", 13, first_parent),
+        )
+
+        res = first_object.group_join(
+            second_object,
+            lambda parent: parent.age,
+            lambda child: child.parent.age
+            if child.parent is not None
+            else None,
+            lambda parent, children: (parent.age, children.source),
+        )
+
+        assert len(res.source) == 1
