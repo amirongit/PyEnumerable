3936a66bf4ce89c9730fcadc53e8f53827abcdf7
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-04-25 23:35:14 +0330

    add generic to hashed pp

--- pyenumerable/implementations/hashed_pure_python.py
+++ pyenumerable/implementations/hashed_pure_python.py
@@ -1,4 +1,8 @@
+from collections.abc import Hashable
+
 from pyenumerable.implementations.pure_python import PurePythonEnumerable
 
 
-class HashedPurePythonEnumerable(PurePythonEnumerable): ...
+class HashedPurePythonEnumerable[TSource: Hashable](
+    PurePythonEnumerable[TSource]
+): ...
