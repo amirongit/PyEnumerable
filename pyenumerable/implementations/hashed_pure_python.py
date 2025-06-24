cb699a90cac47c89afb0e526fe2bb50f6fbf6095
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-06-24 19:02:03 +0330

    remove separate hashed pp impl

--- pyenumerable/implementations/hashed_pure_python.py
+++
@@ -1,8 +0,0 @@
-from collections.abc import Hashable
-
-from pyenumerable.implementations.pure_python import PurePythonEnumerable
-
-
-class HashedPurePythonEnumerable[TSource: Hashable](
-    PurePythonEnumerable[TSource]
-): ...
