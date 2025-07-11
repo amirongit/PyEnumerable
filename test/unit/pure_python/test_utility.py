4e3e497eb1e5039206646c5b006a78c2035e9f6f
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-07-11 23:29:36 +0330

    remove unused util from test pkg

--- test/unit/pure_python/test_utility.py
+++ test/unit/pure_python/test_utility.py
@@ -4,11 +4,6 @@ from dataclasses import dataclass
 from random import choice
 
 
-def generate_random_args(length: int, range_: range) -> tuple[int, ...]:
-    """shouldn't be used inside test cases"""
-    return tuple(choice(range_) for _ in range(length))
-
-
 @dataclass(frozen=True, eq=True)
 class Person:
     name: str
