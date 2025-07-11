077effd98df113a06d4c9abc8588ee400bacc5d2
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-07-11 23:33:57 +0330

    update versions

--- pyenumerable/__init__.py
+++ pyenumerable/__init__.py
@@ -5,7 +5,7 @@ from pyenumerable.protocol import Enumerable
 __all__ = ["Enumerable", "PurePythonEnumerable", "pp_enumerable"]
 __author__ = "AmirHossein Ahmadi"
 __license__ = "WTFPL"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __maintainer__ = "AmirHossein Ahmadi"
 __email__ = "amirthehossein@gmail.com"
 __documentation__ = "https://github.com/amirongit/PyEnumerable/blob/master/documentation.md"
